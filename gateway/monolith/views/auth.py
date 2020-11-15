feea62f647739ade61c2ad3e525e4277f0b28393
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2020-11-15 13:41:31 +0100

    Add control exception from request
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- gateway/monolith/views/auth.py
+++ gateway/monolith/views/auth.py
@@ -19,9 +19,16 @@ def login():
         if form.validate_on_submit():
             email, password = form.data["email"], form.data["password"]
             json_login = {"email": email, "password": password}
-            response = requests.post("{}/login".format(USER_MICROSERVICE_URL),
-                data=json_login,
-            )
+            url = "{}/login".format(USER_MICROSERVICE_URL)
+            current_app.logger.debug("URL to user microservice: {}".format(url))
+            # Look at https://github.com/psf/requests/issues/1198
+            try:
+                response = requests.post(url, data=json_login)
+            except requests.exceptions.ConnectionError:
+                return render_template(
+                    "login.html", form=form, _test="error_login", message="Connection refused",
+                    base_url="http://localhost/ui/login"
+                )
 
             if not response.ok:
                 current_app.logger.error(response.json())
@@ -33,7 +40,7 @@ def login():
             user = UserModel()
             user.fill_from_json(response.json())
             if UserService.log_in_user(user):
-                return redirect("/")
+                return redirect("/ui")
             else:
                 current_app.logger.error("log in failed")
                 return render_template(
