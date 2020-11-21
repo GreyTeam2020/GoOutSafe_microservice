7b86fb9005abec7510325258a90430bbb774ea2c
Renato <Eschini>
2020-11-21 22:59:17 +0100

    add health auth releated methods

--- gateway/monolith/services/healthy_services.py
+++ gateway/monolith/services/healthy_services.py
@@ -1,6 +1,7 @@
 from flask import current_app
 from monolith.utils.http_utils import HttpUtils
 from monolith.app_constant import USER_MICROSERVICE_URL
+from monolith.model import UserModel
 
 from datetime import datetime, timedelta
 from sqlalchemy import extract
@@ -21,7 +22,13 @@ class HealthyServices:
         # bind filter params...
         url = "{}/positive".format(USER_MICROSERVICE_URL)
         response = HttpUtils.make_get_request(url)
-        return response
+        users = response["result"]
+        list_user = []
+        for user in users:
+            new_user = UserModel()
+            new_user.fill_from_json(user)
+            list_user.append(new_user)
+        return list_user
 
     @staticmethod
     def mark_positive(user_email: str = "", user_phone: str = "") -> str:
@@ -39,8 +46,17 @@ class HealthyServices:
 
         '''
         TODO: chiamata contact
-     
-        TODO CHIAMATE PER INVIO EMAIL A email_microservice
+        esempio risposta API contact
+        {
+           "friends":["aaa@aaa.it","bbbb@bbb.it","bbbb@bbb.it"],
+           "contacts":["","",""],
+           "past_restaurants":["","",""],
+           "reservation_restaurants":["","",""]
+        }
+        la risposta viene mandata come request body al email_microservice per notificare i contatti 
+        il servizio prende le 4 liste, per ognuna cicla ed invia una mail ad ogni indirizzo
+        
+        ELEIMINARE, SONO LE VECCHIE CHIAMATE PER INVIO EMAIL, VENGONO MESSE IN email_microservice
          DispatcherMessage.send_message(
                     NEW_COVID_TO_RESTAURANT_BOOKING,
                     [
@@ -50,7 +66,7 @@ class HealthyServices:
                         restaurant.name,
                     ],
                 )
-
+        
 
          DispatcherMessage.send_message(
             NEW_POSITIVE_WAS_IN_RESTAURANT,
@@ -90,3 +106,41 @@ class HealthyServices:
         '''
 
         return response
+
+
+    @staticmethod
+    def unmark_positive(user_email: str, user_phone: str) -> str:
+        """
+        This method mark the a people as positive on db
+        :param user_email:
+        :param user_phone:
+        :return: return a message
+        """
+        if user_email == "" and user_phone == "":
+            return "Insert an email or a phone number"
+
+        if user_email != "":
+            body = str({
+                "key": "email",
+                "value": user_email
+            })
+        else:
+            body = str({
+                "key": "phone",
+                "value": user_phone
+            })
+
+        URL = USER_MICROSERVICE_URL + "/unmark"
+
+        response = HttpUtils.make_put_request(URL, body)
+
+        if response is None:
+            return "An error occurs"
+
+        if response[1] == 400:
+            return "An error occurs, please try again"
+
+        if response[1] == 200:
+            return response[0]["result"]
+
+        return "Error"
